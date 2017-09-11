e4996e864be7d4d28dd695f242b8acd4565a9061
14891A05G2 <31825005+14891A05G2@users.noreply.github.com>
2017-09-11 13:41:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+public class Main {
+
+	public static void main(String[] args) {
+
+		StudentGroup l=new StudentGroup(5);
+
+	Date d=new Date(1997,8,27);
+	Student s1=new Student(80,"lucky",d,91.0);
+	Student s2=new Student(90,"sam",d,89.0);
+
+                l.addFirst(s1);
+
+              System.out.println(stg.getStudent(0).getId());
+
+
+
+
+
+
+	}
+
+}
